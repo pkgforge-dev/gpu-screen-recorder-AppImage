117768ba14cd327544155bb83082c7a84ca428c9
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-06-21 23:51:27 +0000

    Add files via upload

---
+++ get-dependencies.sh
@@ -0,0 +1,107 @@
+#!/bin/sh
+
+set -ex
+
+sed -i 's/DownloadUser/#DownloadUser/g' /etc/pacman.conf
+
+if [ "$(uname -m)" = 'x86_64' ]; then
+	PKG_TYPE='x86_64.pkg.tar.zst'
+else
+	PKG_TYPE='aarch64.pkg.tar.xz'
+fi
+
+LLVM_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/llvm-libs-nano-$PKG_TYPE"
+FFMPEG_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/ffmpeg-mini-$PKG_TYPE"
+LIBXML_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/libxml2-iculess-$PKG_TYPE"
+OPUS_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/opus-nano-$PKG_TYPE"
+MESA_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/mesa-mini-$PKG_TYPE"
+
+echo "Installing dependencies..."
+echo "---------------------------------------------------------------"
+pacman -Syu --noconfirm       \
+	alsa-lib                \
+	base-devel              \
+	cairo                   \
+	cmake                   \
+	curl                    \
+	desktop-file-utils      \
+	ffmpeg                  \
+	gcc-libs                \
+	gdk-pixbuf2             \
+	git                     \
+	glib2                   \
+	glibc                   \
+	gtk3                    \
+	hicolor-icon-theme      \
+	libayatana-appindicator \
+	libglvnd                \
+	libpulse                \
+	libx11                  \
+	libxrandr               \
+	libxss                  \
+	mesa                    \
+	meson                   \
+	pango                   \
+	pipewire-audio          \
+	pulseaudio              \
+	pulseaudio-alsa         \
+	vulkan-headers          \
+	vulkan-icd-loader       \
+	wget                    \
+	xorg-server-xvfb        \
+	zlib                    \
+	zsync
+
+echo "Installing debloated pckages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$LLVM_URL"   -O  ./llvm-libs.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$LIBXML_URL" -O  ./libxml2.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$FFMPEG_URL" -O  ./ffmpeg.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$OPUS_URL"   -O  ./opus.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$MESA_URL"   -O  ./mesa.pkg.tar.zst
+
+pacman -U --noconfirm ./*.pkg.tar.zst
+rm -f ./*.pkg.tar.zst
+
+# Make the thing
+echo "Building gpu-screen-recorder..."
+echo "---------------------------------------------------------------"
+sed -i 's|EUID == 0|EUID == 69|g' /usr/bin/makepkg
+sed -i 's|-O2|-O3|; s|MAKEFLAGS=.*|MAKEFLAGS="-j$(nproc)"|; s|#MAKEFLAGS|MAKEFLAGS|' /etc/makepkg.conf
+cat /etc/makepkg.conf
+
+git clone https://aur.archlinux.org/gpu-screen-recorder.git ./gpu-screen-recorder && (
+	cd ./gpu-screen-recorder
+	# modify gpu-screen-recorder to build without systemd and wihtout caps
+	sed -i 's|-Dsystemd=true|-Dsystemd=false -Dcapabilities=false|' ./PKGBUILD
+	makepkg -f
+	ls -la .
+	pacman --noconfirm -U *.pkg.tar.*
+)
+
+# now the rest
+git clone https://aur.archlinux.org/gpu-screen-recorder-gtk.git ./gpu-gtk && (
+	cd ./gpu-gtk
+	makepkg -f
+	ls -la .
+	pacman --noconfirm -U *.pkg.tar.*
+)
+
+git clone https://aur.archlinux.org/gpu-screen-recorder-notification.git ./notification && (
+	cd ./notification
+	makepkg -f
+	ls -la .
+	pacman --noconfirm -U *.pkg.tar.*
+)
+
+git clone https://aur.archlinux.org/gpu-screen-recorder-ui.git ./gpu-ui && (
+	cd ./gpu-ui
+	makepkg -f
+	ls -la .
+	pacman --noconfirm -U *.pkg.tar.*
+)
+
+rm -rf ./gpu-ui ./notification ./gpu-gtk ./gpu-screen-recorder
+
+echo "All done!"
+echo "---------------------------------------------------------------"
---
+++ gpu-screen-recorder-appimage.sh
@@ -0,0 +1,131 @@
+#!/bin/sh
+
+set -ex
+
+export ARCH="$(uname -m)"
+URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
+URUNTIME_LITE="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-lite-$ARCH"
+UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
+SHARUN="https://github.com/VHSgunzo/sharun/releases/latest/download/sharun-$ARCH-aio"
+
+VERSION="$(pacman -Q gpu-screen-recorder | awk '{print $2; exit}')"
+echo "$VERSION" > ~/version
+
+# Prepare AppDir
+mkdir -p ./AppDir/share/icons/hicolor/32x32 && (
+	cd ./AppDir
+
+	cp -rv /usr/share/gsr-ui                      ./share
+	cp -rv /usr/share/icons/hicolor/32x32/status  ./share/icons/hicolor/32x32
+
+	cp -v /usr/share/applications/com.dec05eba.gpu_screen_recorder.desktop           ./
+	cp -v /usr/share/icons/hicolor/128x128/apps/com.dec05eba.gpu_screen_recorder.png ./
+	cp -v /usr/share/icons/hicolor/128x128/apps/com.dec05eba.gpu_screen_recorder.png ./.DirIcon
+
+	# ADD LIBRARIES
+	wget --retry-connrefused --tries=30 "$SHARUN" -O ./sharun-aio
+	chmod +x ./sharun-aio
+	xvfb-run -a \
+		./sharun-aio l -p -v -e -s -k \
+		/usr/bin/gpu-screen-recorder* \
+		/usr/bin/gsr-dbus-server      \
+		/usr/bin/gsr-notify           \
+		/usr/bin/gsr-*                \
+		/usr/lib/gdk-pixbuf-*/*/*/*   \
+		/usr/lib/pulseaudio/*         \
+		/usr/lib/pipewire-*/*         \
+		/usr/lib/spa-0.2/*/*          \
+		/usr/lib/libavutil.so*        \
+		/usr/lib/libavformat.so*      \
+		/usr/lib/lib*GL*              \
+		/usr/lib/dri/*                \
+		/usr/lib/vdpau/*
+
+	rm -f ./sharun-aio ./bin/gsr-global-hotkeys ./bin/gsr-kms-server
+
+	# hack
+	cp ./sharun  ./sharun2
+	cp ./sharun  ./sharun3
+	ln ./sharun2 ./bin/gsr-global-hotkeys
+	ln ./sharun3 ./bin/gsr-kms-server
+
+	# sus
+	sed -i 's|/usr/share|././/share|g' ./shared/bin/*
+
+	# Prepare sharun
+	cat > ./AppRun <<-'EOF'
+	#!/bin/sh
+	set -e
+	APPDIR="$(cd "${0%/*}" && echo "$PWD")"
+	BIN="${ARGV0:-$0}"
+	BIN="${BIN#./}"
+	unset ARGV0
+
+	mkdir -p /tmp/.gsr-appimage-hack/bin
+
+	# change working dir since we did binary patching
+	cd /tmp/.gsr-appimage-hack
+
+	# hack to get capabities working
+	if [ ! -f /tmp/.gsr-appimage-hack/sharun ]; then
+	        cp "$APPDIR"/sharun* ./
+
+	        ln -f ./sharun  ./bin/gpu-screen-recorder
+	        ln -f ./sharun  ./bin/gpu-screen-recorder-gtk
+	        ln -f ./sharun  ./bin/gsr-dbus-server
+	        ln -f ./sharun  ./bin/gsr-notify
+	        ln -f ./sharun  ./bin/gsr-ui
+	        ln -f ./sharun  ./bin/gsr-cli
+	        ln -f ./sharun2 ./bin/gsr-global-hotkeys
+	        ln -f ./sharun3 ./bin/gsr-kms-server
+	fi
+
+	ln -sfn "$APPDIR"/etc     /tmp/.gsr-appimage-hack
+	ln -sfn "$APPDIR"/lib     /tmp/.gsr-appimage-hack
+	ln -sfn "$APPDIR"/share   /tmp/.gsr-appimage-hack
+	ln -sfn "$APPDIR"/shared  /tmp/.gsr-appimage-hack
+
+	if [ -z "$(getcap /tmp/.gsr-appimage-hack/bin/*)" ]; then
+	        pkexec sh -c 'setcap cap_setuid+ep /tmp/.gsr-appimage-hack/bin/gsr-global-hotkeys \
+	                && setcap cap_sys_admin+ep /tmp/.gsr-appimage-hack/bin/gsr-kms-server'
+	fi
+
+	# This doesn't work because this program uses pidof internall, so when ARGV0 is
+	# gsr-ui the program detects the pidof of the AppImage as the actual binary wtf
+	#if [ -f ./bin/"$BIN" ]; then
+	#        exec ./bin/"$BIN" "$@"
+
+	if [ -n "$1" ] && [ -f ./bin/"$1" ]; then
+	        BIN="$1"
+	        shift
+	        exec ./bin/"$BIN" "$@"
+	else
+	        exec ./bin/gpu-screen-recorder-gtk "$@"
+	fi
+	EOF
+
+	chmod +x ./AppRun
+	./sharun -g
+)
+
+# MAKE APPIAMGE WITH URUNTIME
+wget --retry-connrefused --tries=30 "$URUNTIME"      -O ./uruntime
+wget --retry-connrefused --tries=30 "$URUNTIME_LITE" -O ./uruntime-lite
+chmod +x ./uruntime*
+
+# Add udpate info to runtime
+echo "Adding update information \"$UPINFO\" to runtime..."
+./uruntime-lite --appimage-addupdinfo "$UPINFO"
+
+echo "Generating AppImage..."
+./uruntime --appimage-mkdwarfs -f        \
+	--set-owner 0 --set-group 0          \
+	--no-history --no-create-timestamp   \
+	--compression zstd:level=22 -S26 -B8 \
+	--header uruntime-lite               \
+	-i ./AppDir -o ./gpu-screen-recorder-"$VERSION"-anylinux-"$ARCH".AppImage
+
+echo "Generating zsync file..."
+zsyncmake *.AppImage -u *.AppImage
+
+echo "All Done!"
