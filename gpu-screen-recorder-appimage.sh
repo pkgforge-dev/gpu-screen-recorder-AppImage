cbca1ece3fabdd0fab79ef021734ea0f2fb8958d
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-11 16:45:52 +0000

    use anylinux wrapper scripts to simplify deployment, add self updater (#16)

--- gpu-screen-recorder-appimage.sh
+++ gpu-screen-recorder-appimage.sh
@@ -1,75 +1,46 @@
 #!/bin/sh
 
-set -ex
+set -eux
 
 ARCH="$(uname -m)"
-URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
-URUNTIME_LITE="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-lite-$ARCH"
-UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
-SHARUN="https://github.com/VHSgunzo/sharun/releases/latest/download/sharun-$ARCH-aio"
+URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 
 VERSION="$(pacman -Q gpu-screen-recorder | awk '{print $2; exit}')"
-echo "$VERSION" > ~/version
+[ -n "$VERSION" ] && echo "$VERSION" > ~/version
+
+export ADD_HOOKS="self-updater.bg.hook"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export OUTNAME=gpu-screen-recorder-"$VERSION"-anylinux-"$ARCH".AppImage
+export DESKTOP=/usr/share/applications/com.dec05eba.gpu_screen_recorder.desktop 
+export ICON=/usr/share/icons/hicolor/128x128/apps/com.dec05eba.gpu_screen_recorder.png
+export DEPLOY_OPENGL=1
+export DEPLOY_PIPEWIRE=1
 
 # Prepare AppDir
-mkdir -p ./AppDir/share/icons/hicolor/32x32 && (
-	cd ./AppDir
+mkdir -p ./AppDir/share/icons/hicolor/32x32
+cp -rv /usr/share/gsr-ui                      ./AppDir/share
+cp -rv /usr/share/icons/hicolor/32x32/status  ./AppDir/share/icons/hicolor/32x32
 
-	cp -rv /usr/share/gsr-ui                      ./share
-	cp -rv /usr/share/icons/hicolor/32x32/status  ./share/icons/hicolor/32x32
+# ADD LIBRARIES
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+./quick-sharun /usr/bin/gpu-screen-recorder* /usr/bin/gsr-*
+rm -f ./AppDir/bin/gsr-global-hotkeys ./AppDir/bin/gsr-kms-server
 
-	cp -v /usr/share/applications/com.dec05eba.gpu_screen_recorder.desktop           ./
-	cp -v /usr/share/icons/hicolor/128x128/apps/com.dec05eba.gpu_screen_recorder.png ./
-	cp -v /usr/share/icons/hicolor/128x128/apps/com.dec05eba.gpu_screen_recorder.png ./.DirIcon
+# sus
+sed -i 's|/usr/share|/tmp/._gsr|g' ./AppDir/shared/bin/*
 
-	# ADD LIBRARIES
-	wget --retry-connrefused --tries=30 "$SHARUN" -O ./sharun-aio
-	chmod +x ./sharun-aio
-	xvfb-run -a \
-		./sharun-aio l -p -v -e -s -k \
-		/usr/bin/gpu-screen-recorder* \
-		/usr/bin/gsr-*                \
-		/usr/lib/gdk-pixbuf-*/*/*/*   \
-		/usr/lib/pulseaudio/*         \
-		/usr/lib/pipewire-*/*         \
-		/usr/lib/spa-0.2/*/*          \
-		/usr/lib/libavutil.so*        \
-		/usr/lib/libavformat.so*      \
-		/usr/lib/lib*GL*              \
-		/usr/lib/dri/*                \
-		/usr/lib/vdpau/*              \
-		/usr/lib/gconv/*
-
-	rm -f ./sharun-aio ./bin/gsr-global-hotkeys ./bin/gsr-kms-server
-
-	# hack
-	cp ./sharun  ./sharun2
-	cp ./sharun  ./sharun3
-
-	# sus
-	sed -i 's|/usr/share|/tmp/._gsr|g' ./shared/bin/*
-
-	chmod +x ./AppRun
-	./sharun -g
-)
+# hack
+cp ./AppDir/sharun  ./AppDir/sharun2
+cp ./AppDir/sharun  ./AppDir/sharun3
 
 # MAKE APPIAMGE WITH URUNTIME
-wget --retry-connrefused --tries=30 "$URUNTIME"      -O ./uruntime
-wget --retry-connrefused --tries=30 "$URUNTIME_LITE" -O ./uruntime-lite
-chmod +x ./uruntime*
+wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
+chmod +x ./uruntime2appimage
+./uruntime2appimage
 
-# Add udpate info to runtime
-echo "Adding update information \"$UPINFO\" to runtime..."
-./uruntime-lite --appimage-addupdinfo "$UPINFO"
-
-echo "Generating AppImage..."
-./uruntime --appimage-mkdwarfs -f        \
-	--set-owner 0 --set-group 0          \
-	--no-history --no-create-timestamp   \
-	--compression zstd:level=22 -S26 -B8 \
-	--header uruntime-lite               \
-	-i ./AppDir -o ./gpu-screen-recorder-"$VERSION"-anylinux-"$ARCH".AppImage
-
+# make appbundle
 UPINFO="$(echo "$UPINFO" | sed 's#.AppImage.zsync#*.AppBundle.zsync#g')"
 wget -O ./pelf "https://github.com/xplshn/pelf/releases/latest/download/pelf_$ARCH"
 chmod +x ./pelf
@@ -83,7 +54,6 @@ echo "Generating [dwfs]AppBundle..."
 	--output-to ./gpu-screen-recorder-"$VERSION"-anylinux-"$ARCH".dwfs.AppBundle
 
 echo "Generating zsync file..."
-zsyncmake ./*.AppImage -u ./*.AppImage
 zsyncmake ./*.AppBundle -u ./*.AppBundle
 
 echo "All Done!"
