382bd2fe9351dad1b490e685c94f3602ff0e0e71
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-13 01:19:49 -0400

    add `ffmpeg-mini`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -32,7 +32,7 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-common --prefer-nano intel-media-driver-mini
+./get-debloated-pkgs.sh --add-common --prefer-nano intel-media-driver-mini ffmpeg-mini
 
 # Make the thing
 echo "Building gpu-screen-recorder..."
