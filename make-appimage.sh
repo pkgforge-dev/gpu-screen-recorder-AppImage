0248a5298f78ea0b872e39c4e56d8ffcb7268286
Samueru-sama <xdglawyer@outlook.com>
2026-03-29 02:50:07 -0400

    update to latest template

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,7 +9,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export OUTPATH=./dist
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export APPNAME=gpu-screen-recorder
-export DESKTOP=/usr/share/applications/com.dec05eba.gpu_screen_recorder.desktop 
+export DESKTOP=/usr/share/applications/com.dec05eba.gpu_screen_recorder.desktop
 export ICON=/usr/share/icons/hicolor/128x128/apps/com.dec05eba.gpu_screen_recorder.png
 export DEPLOY_OPENGL=1
 export DEPLOY_PIPEWIRE=1
@@ -32,3 +32,7 @@ cp ./AppDir/sharun  ./AppDir/sharun3
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the test fails due to the app
+# having issues running in the CI use --simple-test instead
+quick-sharun --test ./dist/*.AppImage
